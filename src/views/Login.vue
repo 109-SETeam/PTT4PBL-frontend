96f80b5c2d514c377653de99bc26de71eace529b
weiChen <asdzxc55888@gmail.com>
2020-11-06 15:34:22 +0800

    added login view

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-button class="login-btn" type="info" size="default" @click="handleLogin">透過 Github 登入</el-button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    methods:{
+        handleLogin: () => {
+            console.log("click")
+        }
+    }
+});
+</script>
+
+<style lang="scss">
+.login-btn{
+    background-color: rgb(46, 46, 46);
+}
+</style>
