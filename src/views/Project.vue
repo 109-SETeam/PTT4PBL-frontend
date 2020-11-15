c37c89bd2082cf91a7f653558e5a0ff85b0f6b32
zxjte9411 <a5852241@gmail.com>
2020-11-16 04:54:50 +0800

    add project's view

---
+++ src/views/Project.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- <v-container fluid> -->
+    <v-row class="mb-6" no-gutters>
+      <v-col>
+        <v-card class="pa-2" outlined tile>1/2</v-card>
+      </v-col>
+      <v-col>
+        <v-card class="pa-2" outlined tile>1/2</v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-6" no-gutters>
+      <v-col>
+        <v-card class="pa-2" outlined tile>1/3</v-card>
+      </v-col>
+      <v-col>
+        <v-card class="pa-2" outlined tile>1/3</v-card>
+      </v-col>
+      <v-col>
+        <v-card class="pa-2" outlined tile>1/3</v-card>
+      </v-col>
+    </v-row>
+    <!-- </v-container> -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      project: [],
+      alignments: ["start", "center", "end"]
+    };
+  },
+  methods: {}
+});
+</script>
+
+<style></style>
