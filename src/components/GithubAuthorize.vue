10cba7e148789119e08b979ee7c27ce4f2177147
weiChen <asdzxc55888@gmail.com>
2020-11-17 17:54:21 +0800

    removed unuse

--- src/components/GithubAuthorize.vue
+++ src/components/GithubAuthorize.vue
@@ -13,11 +13,7 @@ export default Vue.extend({
   mounted: () => {
     const query = router.currentRoute.query;
     const code = query.code.toString();
-    //authenticateGithubToken(code).then((res)=>{
-    //    console.log(res)
-    //})
     store.dispatch("login", code);
-    console.log(store.getters.isAuthenticated)
   },
 });
 </script>
