c89abfd812fe1805ccb21079187c8bddb05fdd5d
weiChen <asdzxc55888@gmail.com>
2020-11-17 17:54:44 +0800

    added navbar componet

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+      <NavBar/>
       <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import NavBar from './components/NavBar.vue'
 
-export default Vue.extend({});
+export default Vue.extend({
+  components:{
+    NavBar
+  }
+});
 </script>
 
 <style lang="scss">
