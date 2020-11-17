e2718615fbf384e6c9fb56dd741cf78ec03274d0
weiChen <asdzxc55888@gmail.com>
2020-11-17 17:09:15 +0800

    added vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <router-view />
+      <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({
-});
-
+export default Vue.extend({});
 </script>
 
 <style lang="scss">
