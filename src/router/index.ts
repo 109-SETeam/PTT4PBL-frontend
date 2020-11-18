2e521d852909d39acabab69f761f03224ad6737d
zxjte9411 <a5852241@gmail.com>
2020-11-19 04:52:48 +0800

    add repository vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Login from "@/views/Login.vue"
 import Project from "@/views/Project.vue"
+import Repository from "@/views/Repository.vue"
 import GithubAuthorize from "@/components/GithubAuthorize.vue"
 
 Vue.use(VueRouter);
@@ -18,7 +19,13 @@ const routes: Array<RouteConfig> = [
   },
   {
     path: "/project",
+    name: "Project",
     component: Project
+  },
+  {
+    path: "/project/:id",
+    name: "Repository",
+    component: Repository
   }
 ];
 
---
+++ src/views/Repository.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fluid fill-height class="container-background-color">
+    <v-row> 
+      <v-col lg="6"></v-col>
+      <v-col lg="6"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      repositories: null
+    };
+  },
+  mounted() {
+    this.Test();
+  },
+  methods: {
+    Test() {
+      console.log(this.$route.params.id);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
