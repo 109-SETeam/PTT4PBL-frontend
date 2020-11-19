06a7fe942306e5b4cb9cd415540f70e1b22d1c58
zxjte9411 <a5852241@gmail.com>
2020-11-20 03:13:43 +0800

    修改 api 寫法

--- src/apis/repository.ts
+++ src/apis/repository.ts
@@ -1,19 +1,10 @@
 import axios from "axios";
 import { host } from "../config/config";
 
-export const getRepository: any = async function (
-  projectId: string
-): Promise<any> {
-  return axios
-    .get(`${host}/repository/${projectId}`)
-    .then(function (response) {
-      return response;
-    })
-    .catch(err => {
-      return err;
-    });
-};
+export const getRepository: any = async function (projectId: string): Promise<any> {
+  return axios.get(`${host}/repository/${projectId}`)
+}
 
 export const addRepo: any = async function (projectId: number, url: string): Promise<any> {
-  return axios.post(`${host}/repo`, { projectId, url });//.then(res=>{return res});
+  return axios.post(`${host}/repo`, { projectId, url });
 }
