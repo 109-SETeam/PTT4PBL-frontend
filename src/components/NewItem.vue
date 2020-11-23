0f7b58e88011afa253f3239a82cf5fa71d30848e
willie <finish331520@gmail.com>
2020-11-23 11:16:08 +0800

    新增 新增按鈕的component & 搜尋表格的component

---
+++ src/components/NewItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-dialog v-model="dialog" max-width="60%">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="success" v-bind="attrs" v-on="on" @click="clearInputData"
+        ><v-icon style="background-color: #4caf50 !important">mdi-plus</v-icon
+        >New</v-btn
+      >
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">{{ vCardTitle }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                :label="vTextLabel"
+                v-model="inputData"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Cancel
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="add"> Add </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: ["vCardTitle", "vTextLabel"],
+  data() {
+    return {
+      inputData: "",
+      dialog: false,
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("add", this.inputData);
+      this.dialog = false;
+    },
+    clearInputData() {
+      this.inputData = "";
+    },
+  },
+});
+</script>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -11,74 +11,34 @@
         <v-row>
           <v-col lg="3" class="d-flex align-center text-h3">Project</v-col>
           <v-col lg="7" class="d-flex flex-row">
-            <v-text-field
-              id="table-search-card"
-              class="align-self-end"
-              v-model="searchedName"
-              prepend-inner-icon="mdi-magnify"
-              label="Find a project..."
-              hide-details
-              clearable
-              outlined
-              dense
-              solo
-            ></v-text-field>
+            <TableSearch
+              vTextLabel="Find a Project..."
+              @ChangeInput="ChangeInput($event)"
+            />
           </v-col>
           <v-col lg="2" class="d-flex justify-end align-end">
-            <v-dialog v-model="dialog" max-width="60%">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="success"
-                  v-bind="attrs"
-                  v-on="on"
-                  @click="clearInputProjectName"
-                  ><v-icon style="background-color: #4caf50 !important"
-                    >mdi-plus</v-icon
-                  >New</v-btn
-                >
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">Add Project</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12">
-                        <v-text-field
-                          label="Project Name"
-                          v-model="inputAddedProjectName"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="dialog = false">
-                    Cancel
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="addProject">
-                    Add
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <NewItem
+              vCardTitle="Add Project"
+              vTextLabel="Project Name"
+              @add="addproject($event)"
+            />
           </v-col>
         </v-row>
         <v-divider></v-divider>
         <v-row>
           <v-col lg="12">
-            <DataTable
-              :searchedName="searchedName"
-              :tableData="projects"
-              @goToRepositoryDetail="console.log('2')"
-            />
+            <DataTable :searchedName="searchedName" :tableData="projects" />
           </v-col>
         </v-row>
       </v-col>
       <!-- 右邊表格 end -->
+      <v-snackbar
+        v-model="snackBar"
+        :timeout="snackBarTimeout"
+        :color="snackBarColor"
+      >
+        {{ msg }}
+      </v-snackbar>
     </v-row>
   </v-container>
 </template>
@@ -88,43 +48,54 @@ import Vue from "vue";
 import { addProject, getProject } from "@/apis/projects.ts";
 import UserInfo from "@/components/UserInfo.vue";
 import DataTable from "@/components/DataTable.vue";
+import NewItem from "@/components/NewItem.vue";
+import TableSearch from "@/components/TableSearch.vue";
 
 export default Vue.extend({
   components: {
     UserInfo,
     DataTable,
+    NewItem,
+    TableSearch,
   },
   data() {
     return {
-      inputAddedProjectName: "",
       dialog: false,
       searchedName: "",
       projects: [] as any,
+      msg: "",
+      snackBar: false,
+      snackBarTimeout: 1500,
+      snackBarColor: "",
     };
   },
   mounted() {
     this.getProject();
   },
   methods: {
-    clearInputProjectName() {
-      this.inputAddedProjectName = "";
-    },
     async getProject() {
       console.log(await getProject("test123"));
       this.projects = (await getProject("test123"))["data"];
     },
-    async addProject() {
+    async addproject(inputData: any) {
       //TODO：這邊要做新增專案成功及失敗的處理，成功：關閉dialog並顯示新增成功，失敗：保留dialog，顯示新增失敗
-      const result = await addProject(this.inputAddedProjectName, "test123");
+      const result = await addProject(inputData, "test123");
 
       if (result) {
-        console.log("success");
+        this.msg = "新增成功";
+        this.snackBar = true;
+        this.snackBarColor = "green";
+        this.projects = (await getProject("test123"))["data"];
       } else {
-        console.log("fail");
+        this.msg = "新增失敗";
+        this.snackBar = true;
+        this.snackBarColor = "red";
       }
 
       this.dialog = false;
-      this.projects = (await getProject("test123"))["data"];
+    },
+    ChangeInput(searchedName: any) {
+      this.searchedName = searchedName;
     },
   },
 });
