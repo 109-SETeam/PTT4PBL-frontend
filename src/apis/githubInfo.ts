1803b31202e569ddf3a59b2db0a58184ca1048e0
Willie <t109598008@ntut.org.tw>
2020-11-24 17:57:19 +0800

    added getCodebase and getContributor api

--- src/apis/githubInfo.ts
+++ src/apis/githubInfo.ts
@@ -5,3 +5,11 @@ const octokit = new Octokit({ auth: store === undefined ? undefined : store.gett
 export const getCommitInfo = (owner: string, repo: string) => {
     return octokit.request(`GET /repos/${owner}/${repo}/stats/commit_activity`);
 }
+
+export const getCodebase = (owner: string, repo: string) => {
+    return octokit.request(`GET /repos/${owner}/${repo}/stats/code_frequency`);
+}
+
+export const getContributors = (owner: string, repo: string) => {
+    return octokit.request(`GET /repos/${owner}/${repo}/stats/contributors`);
+}
