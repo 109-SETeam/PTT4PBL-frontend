31ab0453869c814b3fc47497a4c1ab66b9583dec
zxjte9411 <a5852241@gmail.com>
2020-11-25 03:16:06 +0800

    移除多於的宣告

--- src/apis/projects.ts
+++ src/apis/projects.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { host } from "../config/config"
 
-export const getProject: any = async function (encryptUserId: string): Promise<any> {
+export const getProjects: any = () => {
     return axios.get(`${host}/project/${encryptUserId}`).then(function (response) {
         return response
     }).catch(err => {
