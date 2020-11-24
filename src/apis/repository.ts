180de587f344de38a81c3f1a13c6a24f1dbeb868
zxjte9411 <a5852241@gmail.com>
2020-11-25 03:21:28 +0800

    在 headers 中加入 Authorization 資訊

--- src/apis/repository.ts
+++ src/apis/repository.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { host } from "../config/config";
+import store from '@/store';
 
 export const getRepository: any = (projectId: string) => {
-  return axios.get(`${host}/repo/${projectId}`)
+  return axios.get(`${host}/repo/${projectId}`, {
+    headers: {
+      Authorization: `Bearer ${store.getters.token}`
+    }
+  })
 }
 
 export const addRepo: any = (projectId: number, url: string) => {
