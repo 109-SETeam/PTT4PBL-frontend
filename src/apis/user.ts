3520a0bdcb7bdce330220a4492cc5782f756a1cc
zxjte9411 <a5852241@gmail.com>
2020-11-25 03:22:44 +0800

    取得 user 的 name、avatarUrl

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,6 +1,15 @@
 import axios from "axios"
+import store from '@/store';
 import { host } from "../config/config"
 
 export const getUser = () => {
     return axios.get(`${host}/user`)
+}
+
+export const getUserInfo = ():Promise<any> => {
+    return axios.get(`${host}/user`, {
+        headers: {
+            Authorization : `Bearer ${store.getters.token}`
+        }
+    })
 }
