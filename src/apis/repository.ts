72d390d748dcf7c7e76ed67bbdcd293d1bfb0e34
zxjte9411 <a5852241@gmail.com>
2020-11-25 03:21:19 +0800

    移除多於宣告

--- src/apis/repository.ts
+++ src/apis/repository.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { host } from "../config/config";
 
-export const getRepository: any = async function (projectId: string): Promise<any> {
+export const getRepository: any = (projectId: string) => {
   return axios.get(`${host}/repo/${projectId}`)
 }
 
-export const addRepo: any = async function (projectId: number, url: string): Promise<any> {
+export const addRepo: any = (projectId: number, url: string) => {
   return axios.post(`${host}/repo`, { projectId, url });
 }
