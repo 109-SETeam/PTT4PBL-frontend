d82f49c886bd587ee6501ae9b45be4a927de5db5
weiChen <asdzxc55888@gmail.com>
2020-11-24 14:02:01 +0800

    added oauth token in store

--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -4,39 +4,51 @@ import { authenticateGithubToken } from '../../apis/authorize'
 
 interface Auth {
     token: string | null
+    oauthToken: string | null
 }
 
 const state: Auth = {
     token: null,
+    oauthToken: null
 };
+
 const getters = {
     isAuthenticated: (state: Auth) => !!state.token,
-    token: (state: Auth) => state.token
+    token: (state: Auth) => state.token,
+    oauthToken: (state: Auth) => state.oauthToken,
 };
+
 const actions = {
     async login(context: any, githubCode: string) {
         authenticateGithubToken(githubCode).then((res) => {
-            const token = res.data;
+            const token = res.data.token;
+            const oauthToken = res.data.oauthToken;
             context.commit('setToken', token);
+            context.commit('setOauthToken', oauthToken);
             axios.defaults.headers.common['Authorization'] = token;
             router.push('/');
         }).catch((err) => {
-            console.log(err);
             alert("系統發生錯誤！");
             router.push('/');
         });
     },
     logout(context: any) {
         context.commit('setToken', null);
+        context.commit('setOauthToken', null);
         axios.defaults.headers.common['Authorization'] = null;
         router.push('/');
     }
 };
+
 const mutations = {
     setToken(state: Auth, token: string) {
         state.token = token
+    },
+    setOauthToken(state: Auth, oauthToken: string) {
+        state.oauthToken = oauthToken
     }
 };
+
 export default {
     state,
     getters,
