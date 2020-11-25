af3d4a7faab61dd645c1d7daec946850ae66bb1c
weiChen <asdzxc55888@gmail.com>
2020-11-25 16:21:51 +0800

    removed githubInfo api file

--- src/apis/githubInfo.ts
+++
@@ -1,7 +0,0 @@
-import store from '@/store';
-import { Octokit } from "@octokit/rest";
-const octokit = new Octokit({ auth: store === undefined ? undefined : store.getters.oauthToken });
-
-export const getCommitInfo = (owner: string, repo: string) => {
-    return octokit.request(`GET /repos/${owner}/${repo}/stats/commit_activity`);
-}
