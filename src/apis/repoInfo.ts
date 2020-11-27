7d6ac9641a6c1ff545235348e4a88d6d9c7127c0
Willie <t109598008@ntut.org.tw>
2020-11-27 18:26:31 +0800

    Cascaded getCodebase api

--- src/apis/repoInfo.ts
+++ src/apis/repoInfo.ts
@@ -4,3 +4,7 @@ import { host } from "../config/config"
 export const getCommitInfo = (repoId:number|null) =>{
     return axios.get(`${host}/repoInfo/commit/${repoId}`);
 }
+
+export const getCodebase = (repoId:number|null) =>{
+    return axios.get(`${host}/repoInfo/codebase/${repoId}`);
+}
