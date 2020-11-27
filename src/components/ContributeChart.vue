65ef2ae4f13159efc577773c06ba0f440a308a6d
zxjte9411 <a5852241@gmail.com>
2020-11-28 07:02:46 +0800

    完成

--- src/components/ContributeChart.vue
+++ src/components/ContributeChart.vue
@@ -1,31 +1,76 @@
 <template>
-  <v-row justify="space-around">
-    <v-card v-for="(item, index) in data" :key="index" width="300" height="380">
-      <v-card-title class="mb-1">
-        <v-avatar size="60">
-          <img alt="user" :src="item.author.avatar_url" />
-        </v-avatar>
-        <v-card-subtitle class="text-left">
-          <a
-            class="text-subtitle-1 font-weight-regular"
-            target="aboutblank"
-            :href="item.author.html_url"
-            >{{ item.author.login }}</a
-          >
-        </v-card-subtitle>
-      </v-card-title>
+  <v-container>
+    <v-row class="d-flex justify-end">
+      <div class="text-center my-3">
+        <v-menu offset-y transition="scale-transition">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on" width="150">
+              {{ selected }}
+            </v-btn>
+          </template>
+          <v-list>
+            <template v-for="item in activities">
+              <v-list-item :key="item.name" link class="pa-0">
+                <v-list-item-content class="pa-0">
+                  <v-btn class="pa-0" color="white" @click="Test(item)">{{
+                    item.name
+                  }}</v-btn>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row>
+      <template v-for="(item, index) in data">
+        <v-col lg="4" class="d-flex justify-center" :key="item.total">
+          <v-card :key="index" width="450" height="380" class="my-2">
+            <v-card-title class="mb-1">
+              <v-avatar size="60">
+                <img alt="user" :src="item.author.avatar_url" />
+              </v-avatar>
+              <div class="justify-space-around">
+                <v-card-subtitle class="text-left py-0">
+                  <a
+                    class="text-subtitle-1 font-weight-regular"
+                    target="aboutblank"
+                    :href="item.author.html_url"
+                    >{{ item.author.login }}</a
+                  >
+                </v-card-subtitle>
+                <div class="d-flex justify-space-around">
+                  <v-card-subtitle class="text-left py-0"
+                    ><a class="grey--text" href="">
+                      commits {{ item.total }}</a
+                    ></v-card-subtitle
+                  >
+                  <v-card-subtitle class="text-left py-0 green--text"
+                    >{{ item.totalAdditions }} ++</v-card-subtitle
+                  >
+                  <v-card-subtitle class="text-left py-0 red--text"
+                    >{{ item.totalDeletions }} --</v-card-subtitle
+                  >
+                </div>
+              </div>
+            </v-card-title>
 
-      <ve-line
-        width="300px"
-        height="300px"
-        :legend-visible="false"
-        :y-axis="yaxis"
-        :data="chartData[index]"
-        :settings="chartSettings"
-        :colors="['#fb8532']"
-      ></ve-line>
-    </v-card>
-  </v-row>
+            <v-divider class="mx-4"></v-divider>
+            <ve-line
+              height="300px"
+              :legend-visible="false"
+              :loading="loading"
+              :y-axis="yaxis"
+              :data="chartData[index]"
+              :settings="chartSettings"
+              :colors="['#fb8532']"
+            ></ve-line>
+          </v-card>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -50,12 +95,18 @@ export default Vue.extend({
   },
   data() {
     return {
+      loading: false,
+      selected: "commit",
+      activities: [
+        { mode: "c", name: "commit", lineHight: 40 },
+        { mode: "a", name: "additions", lineHight: 10000 },
+        { mode: "d", name: "deletions", lineHight: 1000 },
+      ],
       yaxis: {
         max: 40,
       },
       data: {} as { author: string; total: number; weeks: Rows[] }[],
       chartSettings: {
-        // metrics: ["commit"],
         labelMap: {
           ws: "day",
           c: "commits",
@@ -69,6 +120,19 @@ export default Vue.extend({
     };
   },
   methods: {
+    Test(something: any) {
+      this.loading = true;
+      this.chartData = [];
+      this.selected = something.name;
+      this.yaxis.max = something.lineHight;
+      this.data.forEach((item, index) => {
+        this.chartData.push({
+          rows: item.weeks,
+          columns: ["ws", something.mode],
+        });
+      });
+      this.loading = false;
+    },
     getContributeInfo() {
       getContributeInfo(this.repoId)
         .then((response) => {
@@ -83,18 +147,6 @@ export default Vue.extend({
         .catch((err) => {
           alert("系統錯誤");
         });
-    },
-    reConstructContributeInfo(data: Rows[]): any {
-      const rows: any[] = [];
-      data.forEach((week) => {
-        rows.push({
-          day: week.ws,
-          commits: week.c,
-          additions: week.a,
-          deletions: week.d,
-        });
-      });
-      return rows;
     },
   },
   mounted() {
