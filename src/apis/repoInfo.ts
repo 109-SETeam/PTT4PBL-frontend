f4554fe405b2ffbe484e28da9146913a06d0b078
zxjte9411 <a5852241@gmail.com>
2020-11-28 03:50:44 +0800

    加入驗證 header

--- src/apis/repoInfo.ts
+++ src/apis/repoInfo.ts
@@ -1,10 +1,15 @@
 import axios from "axios"
 import { host } from "../config/config"
+import store from '@/store';
 
-export const getCommitInfo = (repoId:number|null) =>{
+export const getCommitInfo = (repoId: number | null) => {
     return axios.get(`${host}/repoInfo/commit/${repoId}`);
 }
 
-export const getContributeInfo = (repoId:number|null) =>{
-    return axios.get(`${host}/repoInfo/contribute/${repoId}`);
+export const getContributeInfo = (repoId: number | null) => {
+    return axios.get(`${host}/repoInfo/contribute/${repoId}`, {
+        headers: {
+            Authorization: `Bearer ${store.getters.token}`
+        }
+    });
 }
